8a6d3be51d954a07deab02e9a8388b34ed0161b9
Cole Boyer <coleboyer2013@gmail.com>
2025-09-22 13:21:29 -0700

    refactored some stuff into blueprints

--- app/__init__.py
+++ app/__init__.py
@@ -18,5 +18,7 @@ login.login_view = 'login'
 #blueprints
 from app.errors import bp as errors_bp
 app.register_blueprint(errors_bp)
+from app.auth import bp as auth_bp
+app.register_blueprint(auth_bp, url_prefix='/auth')
 
 from app import routes, models
--- app/routes.py
+++ app/routes.py
@@ -40,51 +40,6 @@ def project():
     return render_template('project.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    form = LoginForm()
-    if form.validate_on_submit():
-        user = db.session.scalar( sa.select(User).where(User.username == form.username.data) )
-        if user is None or not user.check_password(form.password.data):
-            flash('Invalid username or password')
-            return redirect(url_for('login'))
-
-        login_user(user, remember=form.remember_me.data)
-        next_page = request.args.get('next')
-        
-        if not next_page or urlsplit(next_page).netloc != '':
-            next_page = url_for('home')
-        
-        return redirect(next_page)
-
-    return render_template('login.html', form=form)
-
-
-@app.route('/logout')
-def logout():
-    logout_user()
-    return redirect(url_for('index'))
-
-
-@app.route('/register', methods=['GET', 'POST'])
-def register():
-    if current_user.is_authenticated:
-        return redirect(url_for('home'))
-
-    form = RegistrationForm()
-    if form.validate_on_submit():
-        user = User(username=form.username.data, email=form.email.data, icon='default')
-        user.set_password(form.password.data)
-        db.session.add(user)
-        db.session.commit()
-        flash('Congratz, you are now a registered user!')
-        return redirect(url_for('login'))
-    return render_template('register.html', form=form)
-
-
 @app.route('/world/create', methods=['GET', 'POST'])
 @login_required
 def create_world():
